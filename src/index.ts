8ff42ffef947391ff7cef3c957ba7ad8abb60986
Guo Yunhe <i@guoyunhe.me>
2024-02-01 21:27:30 +0800

    export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as chunk } from './chunk';
+export { default as difference } from './difference';
+export { default as differenceBy } from './differenceBy';
 export { default as isEqual } from './isEqual';
 export { default as now } from './now';
